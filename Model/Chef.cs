caf61070123be602ada4cefee835d0a813d6f9fb
Martin Slot <slot@sigsysd.k>
2012-02-25 13:07:06 +0100

    Init

---
+++ Model/Chef.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Chef
+    {
+        public string Name { get; private set; }
+        private ICollection<Worker> _workers = new List<Worker>();
+
+        public Chef(string name)
+        {
+            Name = name;
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            if (!_workers.Contains(worker))
+            {
+
+            }
+        }
+
+
+        public ICollection<Worker> GetWorkers()
+        {
+            throw new Exception();
+            return _workers;
+        }
+
+        public void RemoveWorker(Worker worker)
+        {
+            _workers.Remove(worker);
+        }
+    }
+}
---
+++ Test/ModelTest/ChefWorkerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Test.ModelTest
+{
+    [TestFixture]
+    class ChefWorkerTest
+    {
+        [Test]
+        public void AddWorkerToChefTest()
+        {
+            Model.Chef chef = new Model.Chef("Martin");
+            Model.Worker worker1 = new Model.Worker();
+            Model.Worker worker2 = new Model.Worker();
+
+            chef.AddWorker(worker1);
+            chef.AddWorker(worker2);
+
+            Assert.True(chef.GetWorkers().Count == 2);
+            Assert.True(worker1.Chef == chef);
+        }
+    }
+}
